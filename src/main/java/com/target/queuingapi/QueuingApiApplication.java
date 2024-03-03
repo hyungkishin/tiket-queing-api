539c10838c719f1d56c3d107885551ada2c6b8d9
hyungkishin <tkaqkeldk@nate.com>
2024-03-04 00:06:33 +0900

    chore: initial commit

---
+++ src/main/java/com/target/queuingapi/QueuingApiApplication.java
@@ -0,0 +1,13 @@
+package com.target.queuingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuingApiApplication.class, args);
+    }
+
+}
