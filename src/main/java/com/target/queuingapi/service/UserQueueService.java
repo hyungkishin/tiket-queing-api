cb215976b4a27b0753bbcf9d941144bee9bf628c
hyungkishin <tkaqkeldk@nate.com>
2024-03-26 23:44:29 +0900

    feat: 대기열 스케줄러 개발 -1

--- src/main/java/com/target/queuingapi/service/UserQueueService.java
+++ src/main/java/com/target/queuingapi/service/UserQueueService.java
@@ -1,15 +1,23 @@
 package com.target.queuingapi.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.zset.Tuple;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 import java.util.Objects;
 
 import static com.target.queuingapi.exception.ErrorCode.QUEUE_ALREADY_REGISTERED_USER;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
@@ -17,6 +25,8 @@ public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
 
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
 
@@ -49,6 +59,21 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser() {
+        log.info("called scheduling...");
+
+        var maxAllowUserCount = 3L;
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())));
     }
 
 }
